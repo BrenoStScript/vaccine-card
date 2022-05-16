1b79598ace228fc5221ce6ae0676c2072f45d14c
Breno Santos <brenostscript@gmail.com>
2022-05-16 12:22:39 -0300

    commit inicial

---
+++ vacinas.js
@@ -0,0 +1,54 @@
+function calculateAge(){
+
+    var born = document.getElementById('born').value
+    const today = new Date()
+    const date = new Date(born)
+    var ageindays = Math.ceil( (today-date)/86400000 )
+    let atBirth = document.getElementsByName('atBirth')
+    let twoMonths = document.getElementsByName('2M')
+    
+    if(date>today){
+        alert("Data muito avançada!")
+    } else {
+        if(ageindays <= 30){
+             atBirth.forEach(shot=>{
+                shot.style.backgroundColor = 'green'
+            })
+        } else if(ageindays>30){
+            atBirth.forEach(shot=>{
+                shot.style.backgroundColor = 'red'
+            })
+        }
+
+        if(ageindays<45){
+            twoMonths.forEach(shot=>{
+                shot.style.backgroundColor = 'yellow'
+                shot.innerText = 'Aplicar depois do dia '
+            })
+           
+        }else if(ageindays>45 && ageindays<105){
+            twoMonths.forEach(shot=>{
+                shot.style.backgroundColor = 'green'
+                shot.innerText = 'Aplicar até dia: '
+            })
+        }else{
+            twoMonths.forEach(shot=>{
+                shot.style.backgroundColor ='red'
+                shot.innerText = 'Dose perdida!'
+            })
+            }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
